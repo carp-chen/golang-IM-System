e2b6f63860ffff7a938a5d97d2423333ef285030
chen-liyu <252697194@qq.com>
2022-01-11 20:49:12 +0800

    用户上线及广播功能

--- server.go
+++ server.go
@@ -3,28 +3,70 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
-
 type Server struct {
-	Ip string
+	Ip   string
 	Port int
+
+	//在线用户的列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	//消息广播的channel
+	Message chan string
 }
 
 //創建一个server的接口
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip: ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 
 	return server
 }
 
+//监听Message广播消息channel的goroutine，一旦有消息就发送给全部的在线user
+func (this *Server) ListenMessager() {
+	for {
+		msg := <-this.Message
 
-func(this *Server) Handler(conn net.Conn) {
+		//将msg发送给全部的在线user
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+
+//广播消息的方法
+func (this *Server) Broadcast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+
+	this.Message <- sendMsg
+}
+
+func (this *Server) Handler(conn net.Conn) {
 	//...当前连接的业务
-	fmt.Println("连接建立成功")
+	// fmt.Println("连接建立成功")
+
+	user := NewUser(conn)
+
+	// 用户上线，将用户加入到OnlineMap中
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	//广播用户上线消息
+	this.Broadcast(user, "已上线")
+
+	//当前handler
+	select {}
 }
 
 //启动服务器的接口
@@ -32,25 +74,25 @@ func (this *Server) Start() {
 	//socket Listen
 	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
 	if err != nil {
-		fmt.Println("net.Listen err:",err)
+		fmt.Println("net.Listen err:", err)
 		return
 	}
 	//cloce listen socket
 	defer listener.Close()
 
-	for{
+	//启动监听Message的goroutine
+	go this.ListenMessager()
+
+	for {
 		//accpet
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("Listener accept err:",err)
+			fmt.Println("Listener accept err:", err)
 			continue
 		}
 
 		//do handler
 		go this.Handler(conn)
 	}
-
-
-	
 
 }
