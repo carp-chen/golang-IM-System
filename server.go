9c3eb1dbeb26cd9e4404231929e9982b1c08607c
chen-liyu <252697194@qq.com>
2022-01-11 22:45:24 +0800

    用户业务封装

--- server.go
+++ server.go
@@ -56,15 +56,9 @@ func (this *Server) Handler(conn net.Conn) {
 	//...当前连接的业务
 	// fmt.Println("连接建立成功")
 
-	user := NewUser(conn)
+	user := NewUser(conn, this)
 
-	// 用户上线，将用户加入到OnlineMap中
-	this.mapLock.Lock()
-	this.OnlineMap[user.Name] = user
-	this.mapLock.Unlock()
-
-	//广播用户上线消息
-	this.Broadcast(user, "已上线")
+	user.Online()
 
 	//接受客户端发送的消息
 	go func() {
@@ -72,7 +66,7 @@ func (this *Server) Handler(conn net.Conn) {
 		for {
 			n, err := conn.Read(buf)
 			if n == 0 {
-				this.Broadcast(user, "下线")
+				user.Offline()
 				return
 			}
 
@@ -84,8 +78,8 @@ func (this *Server) Handler(conn net.Conn) {
 			//提取用户的消息(去除'\n')
 			msg := string(buf[:n-1])
 
-			//将得到的消息进行广播
-			this.Broadcast(user, msg)
+			//用户针对msg进行消息处理
+			user.DoMessage(msg)
 		}
 	}()
 
--- user.go
+++ user.go
@@ -7,10 +7,12 @@ type User struct {
 	Addr string
 	C    chan string
 	conn net.Conn
+
+	server *Server
 }
 
 //创建一个用户的API
-func NewUser(conn net.Conn) *User {
+func NewUser(conn net.Conn, server *Server) *User {
 	userAddr := conn.RemoteAddr().String()
 
 	user := &User{
@@ -18,12 +20,42 @@ func NewUser(conn net.Conn) *User {
 		Addr: userAddr,
 		C:    make(chan string),
 		conn: conn,
+
+		server: server,
 	}
 
 	//启动监听当前user channel消息的goroutine
 	go user.ListenMessage()
 
 	return user
+}
+
+//用户上线的业务
+func (this *User) Online() {
+
+	// 用户上线，将用户加入到OnlineMap中
+	this.server.mapLock.Lock()
+	this.server.OnlineMap[this.Name] = this
+	this.server.mapLock.Unlock()
+
+	//广播用户上线消息
+	this.server.Broadcast(this, "已上线")
+}
+
+//用户下线的业务
+func (this *User) Offline() {
+	// 用户上线，将用户从OnlineMap中删除
+	this.server.mapLock.Lock()
+	delete(this.server.OnlineMap, this.Name)
+	this.server.mapLock.Unlock()
+
+	//广播用户下线消息
+	this.server.Broadcast(this, "下线")
+}
+
+//用户处理消息的业务
+func (this *User) DoMessage(msg string) {
+	this.server.Broadcast(this, msg)
 }
 
 //监听当前user channel的方法，一旦有消息，就直接发送给对端客户端
