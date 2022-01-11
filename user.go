a2c4991afeaa643e7efd0b89fb0f49a52ec14c0b
chen-liyu <252697194@qq.com>
2022-01-11 23:30:06 +0800

    在线用户查询

--- user.go
+++ user.go
@@ -53,9 +53,24 @@ func (this *User) Offline() {
 	this.server.Broadcast(this, "下线")
 }
 
+//给当前User对应的客户端发送消息
+func (this *User) SendMsg(msg string) {
+	this.conn.Write([]byte(msg))
+}
+
 //用户处理消息的业务
 func (this *User) DoMessage(msg string) {
-	this.server.Broadcast(this, msg)
+	if msg == "who" {
+		//查询当前在线用户都有哪些
+		this.server.mapLock.Lock()
+		for _, user := range this.server.OnlineMap {
+			onlineMsg := "[" + user.Addr + "] " + user.Name + ":" + "在线...\n"
+			this.SendMsg(onlineMsg)
+		}
+		this.server.mapLock.Unlock()
+	} else {
+		this.server.Broadcast(this, msg)
+	}
 }
 
 //监听当前user channel的方法，一旦有消息，就直接发送给对端客户端
