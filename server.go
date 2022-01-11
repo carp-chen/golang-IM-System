ed2b3092e0492be838ef8533dc7d7a6097dcd4f4
chen-liyu <252697194@qq.com>
2022-01-11 22:18:34 +0800

    用户消息广播功能

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
@@ -64,6 +65,29 @@ func (this *Server) Handler(conn net.Conn) {
 
 	//广播用户上线消息
 	this.Broadcast(user, "已上线")
+
+	//接受客户端发送的消息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				this.Broadcast(user, "下线")
+				return
+			}
+
+			if err != nil && err != io.EOF {
+				fmt.Println("Conn Read err:", err)
+				return
+			}
+
+			//提取用户的消息(去除'\n')
+			msg := string(buf[:n-1])
+
+			//将得到的消息进行广播
+			this.Broadcast(user, msg)
+		}
+	}()
 
 	//当前handler
 	select {}
